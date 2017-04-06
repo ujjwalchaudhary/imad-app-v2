21011022938c03e3b641a023e44718796bf697ef
ujjwalchaudhary <uchaudhary244@gmail.com>
2017-04-06 12:53:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,3 +4,9 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 
 element.innerHTML = "New value";
+
+//move the image
+var img = document.getElementById('madi');
+img.onclick = function(){
+  img.style.margineLeft = '100px';  
+};
