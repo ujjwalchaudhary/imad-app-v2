14da2df3733647330f5ccaf257272da215ffa2e1
ujjwalchaudhary <uchaudhary244@gmail.com>
2017-04-06 12:58:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ var element = document.getElementById('main-text');
 
 element.innerHTML = 'New value';
 
-//move the image
+//Move the image
 var img = document.getElementById('madi');
 img.onclick = function(){
   img.style.margineLeft = '100px';  
