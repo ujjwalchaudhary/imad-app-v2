9d32ce4b4b3a253e5fab287974dccf0e8bbd1fac
ujjwalchaudhary <uchaudhary244@gmail.com>
2017-04-06 15:54:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,5 +8,5 @@ function moveRight(){
     img.style.marginLeft = marginLeft+ 'px';
 }
 img.onclick = function() {
-  var interval = setInterval(moveRight,'5px');  
+  var interval = setInterval(moveRight,'60px');  
 };
