c06bcd401b742389aa075948077b0b50ad7e2684
ujjwalchaudhary <uchaudhary244@gmail.com>
2017-04-09 12:22:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,8 +27,6 @@ button.onclick = function() {
 };
 
 //Submit name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
     
@@ -56,6 +54,8 @@ submit.onclick = function(){
     };
     
     //Make the request
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;
     request.open('GET','http://ujjwalchaudhary.imad.hasura-app.io/submit-name?name='+name,true);
     request.send(null);
     
