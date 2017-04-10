7f65f63c9cfec0dc4bd4f665d87b184bc5afe5e6
ujjwalchaudhary <uchaudhary244@gmail.com>
2017-04-10 12:21:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -12,7 +12,7 @@ button.onclick = function() {
         if(request.readyState === XMLHttpRequest.DONE){
             //Take some action
             if(request.status === 200) {
-                var counter = request.ResponseText;
+                var counter = request.responseText;
                 var span = document.getElementById('count');
                 span.innerHTML = counter.toString();
             }
@@ -30,7 +30,7 @@ button.onclick = function() {
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
     
-     //Create a request object
+  //Create a request object
     var request = new XMLHttpRequest();
     
     //Capture the response and store it in variable
@@ -38,21 +38,21 @@ submit.onclick = function(){
         if(request.readyState === XMLHttpRequest.DONE){
             //Take some action
             if(request.status === 200) {
-                //Capture a list of a name and render it as a list
-    var names = request.responseText;
-    names = JSON.parse(names);
-    var list = '';
-    for (var i=0; i< name.lenght; i++) {
-        list += '<li>' + name[i]+ '</li>';
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
+               //Capture a list of names and render it as a list
+               var names = request.responseText;
+               names = JSON.parse(names);
+               var list = '';
+               for(var i=0; i<names.lenght; i++){
+                   list +='<li>'+names[i]'</li>';
+               }
+               var ul = document.getElementById('namelist');
+               ul.innerHTML = list;
             }
+            
         }
         //Not done yet
         
     };
-    
     //Make the request
     var nameInput = document.getElementById('name');
     var name = nameInput.value;
