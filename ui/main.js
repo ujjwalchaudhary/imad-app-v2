e91c30dd4e108a446be15b7cc8960c5f458dca7b
ujjwalchaudhary <uchaudhary244@gmail.com>
2017-04-10 12:24:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -43,7 +43,7 @@ submit.onclick = function(){
                names = JSON.parse(names);
                var list = '';
                for(var i=0; i<names.lenght; i++){
-                   list +='<li>'+names[i]'</li>';
+                   list +='<li>'+names[i]+'</li>';
                }
                var ul = document.getElementById('namelist');
                ul.innerHTML = list;
