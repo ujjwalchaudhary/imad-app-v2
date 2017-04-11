9ae198e3c615d4a46a123f8f15cdb98e02fda8d6
ujjwalchaudhary <uchaudhary244@gmail.com>
2017-04-11 16:54:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var poll = require('pg').pool;
+
+var config = {
+    user: 'ujjwalchaudhary',
+    database: 'ujjwalchaudhary',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: 'db-ujjwalchaudhary-40936',
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -86,6 +95,17 @@ var htmlTemplate=`
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+var pool = new Pool(config)
+app.get('/test-db', function(req, res){
+    //make a select request
+    //return a response with the result
+    pool.query('SELECT * FROM test', function(err, result){
+        if(err){
+            res.status(JSON.stringify(result));
+        }
+    });
 });
 
 var counter = 0;
