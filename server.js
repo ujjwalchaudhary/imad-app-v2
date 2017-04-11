c895f2c7f82a0abd883e779a06e4a879df1cc711
ujjwalchaudhary <uchaudhary244@gmail.com>
2017-04-11 21:26:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -126,12 +126,25 @@ app.get('/submit-name', function(req,res){ // /submit-name?name=xxxxx
     res.send(JSON.stringify(names));
 });
 
-app.get('/:articleName', function(req, res){
+app.get('/articles/:articleName', function(req, res){
     //articleName == article-one
     //articles[articleName] == {}content object for article one
-    var articleName = req.params.articleName;
-    res.send(createTemplate(articles[articleName]));
+    
+    pool.quer("SELECT * FROM article WHERE title = "+ req.params.articleName, function(err, result) {
+        if (err){
+            res.status(500).send(err.toString());
+        } else {
+            if (result.rows.lenght === 0){
+                res.status(400).send('Article not found');
+            } else {
+                var articleData = result.rows[0];
+                res.send(createTemplate(articleData));
+            }
+        }
+    }); 
 });
+    
+   
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
