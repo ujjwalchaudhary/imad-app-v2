ff6004bd6f9ee0d155898f6cfbf7bb3e49c11622
ujjwalchaudhary <uchaudhary244@gmail.com>
2017-04-12 11:38:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
+var crypto = require('crypto');
+
+
 
 var config = {
     user: 'ujjwalchaudhary',
@@ -56,6 +59,21 @@ var htmlTemplate=`
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+function hash(input, salt) {
+    // How do we create a hash?
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
+    return ["pbkdf2", "10000", salt, hashed.toString('hex')].join('$');
+}
+
+
+app.get('/hash/:input', function(req, res){
+    var hashedString = hash(req, param,string, 'this-is-some-random-string');
+    res.send(hashedString);
+});
+
+
+
 
 var pool = new Pool(config)
 app.get('/test-db', function(req, res){
