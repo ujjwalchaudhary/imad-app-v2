3dbac2017a301bb5c3e7420fc87493d847231e58
ujjwalchaudhary <uchaudhary244@gmail.com>
2017-04-12 10:59:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -131,8 +131,8 @@ app.get('/articles/:articleName', function(req, res){
     //articles[articleName] == {}content object for article one
     
     
-    //SELECT * FROM article WHERE title  = 'article-one'
-    pool.query("SELECT * FROM article WHERE title = '"+ req.params.articleName + "'", function(err, result) {
+    //SELECT * FROM article WHERE title  = '\'; DELETE WHERE a = \'asdf'
+    pool.query("SELECT * FROM article WHERE title = $1",[req.params.articleName], function(err, result) {
         if (err){
             res.status(500).send(err.toString());
         } else {
