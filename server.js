69b3c48f133552f386963b082717498bb19c477c
ujjwalchaudhary <uchaudhary244@gmail.com>
2017-04-13 19:56:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -136,16 +136,9 @@ app.post('/login', function(req, res){
 
 app.get('/check-login', function (req, res) {
    if (req.session && req.session.auth && req.session.auth.userId) {
-       // Load the user object
-       pool.query('SELECT * FROM "user" WHERE id = $1', [req.session.auth.userId], function (err, result) {
-           if (err) {
-              res.status(500).send(err.toString());
-           } else {
-              res.send(result.rows[0].username);    
-           }
-       });
+       res.send('You are logged in:' +req.session.auth.userId.toString());
    } else {
-       res.status(400).send('You are not logged in');
+       res.send('You are not logged in');
    }
 });
 
