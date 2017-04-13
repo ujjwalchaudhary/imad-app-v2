01e62e767ec677d134170c7d5cc13a1dce1a591c
ujjwalchaudhary <uchaudhary244@gmail.com>
2017-04-13 18:15:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -78,7 +78,7 @@ app.get('/hash/:input', function(req, res){
 
 app.post('/create-user', function(req, res) {
     //username, password
-    //{"username": "ujjwal", "password": "password"}
+    //{"username": "sajal", "password": "password"}
     //JSON
     var username = req.body.username;
     var password = req.body.password;
