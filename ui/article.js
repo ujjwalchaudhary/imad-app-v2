63a17be57f4e5b0cfa56000b1f3c0f0dc729c408
ujjwalchaudhary <uchaudhary244@gmail.com>
2017-04-14 09:36:00 +0530

    Update article.js

--- ui/article.js
+++ ui/article.js
@@ -1,5 +1,5 @@
 
-// Eg: coco98.imad.hasura-app.io/articles/article-one will result in article-one
+// Eg: ujjwalchaudhary.imad.hasura-app.io/articles/article-one will result in article-one
 var currentArticleTitle = window.location.pathname.split('/')[2];
 
 function loadCommentForm () {
