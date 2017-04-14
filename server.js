3b49d3b46b1a02d00d6cb9d4e1693470630ca68c
ujjwalchaudhary <uchaudhary244@gmail.com>
2017-04-14 11:40:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -144,7 +144,7 @@ app.get('/check-login', function (req, res) {
 
 app.get('/logout', function (req, res) {
    delete req.session.auth;
-   res.send('<html><body>Logged out!<br/><br/><a href="/">Back to home</a></body></html>');
+   res.send('Logout');
 });
 
 
