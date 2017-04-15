d895a57ee481714027db438313a68e3b0122ead7
ujjwalchaudhary <uchaudhary244@gmail.com>
2017-04-15 09:46:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,3 @@
-
 function loadLoginForm () {
     var loginHtml = `
         <h3>Login/Register to unlock awesome features</h3>
@@ -80,12 +79,12 @@ function loadLoginForm () {
     };
 }
 
-function LoadLoggedInUser(username) {
+function loadLoggedInUser (username) {
     var loginArea = document.getElementById('login_area');
-    loginArea.innerHTML =` <h3> Hi <i>${username}</i></h3>
-        <a href="/logout">Logout</a>`;
-       
-   
+    loginArea.innerHTML = `
+        <h3> Hi <i>${username}</i></h3>
+        <a href="/logout">Logout</a>
+    `;
 }
 
 function loadLogin () {
@@ -94,19 +93,45 @@ function loadLogin () {
     request.onreadystatechange = function () {
         if (request.readyState === XMLHttpRequest.DONE) {
             if (request.status === 200) {
-                LoadLoggedInUser(this.responseText);
+                loadLoggedInUser(this.responseText);
             } else {
                 loadLoginForm();
             }
         }
     };
- 
- request.open('GET', '/check-login', true);
+    
+    request.open('GET', '/check-login', true);
     request.send(null);
+}
+
+function loadArticles () {
+        // Check if the user is already logged in
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            var articles = document.getElementById('articles');
+            if (request.status === 200) {
+                var content = '<ul>';
+                var articleData = JSON.parse(this.responseText);
+                for (var i=0; i< articleData.length; i++) {
+                    content += `<li>
+                    <a href="/articles/${articleData[i].title}">${articleData[i].heading}</a>
+                    (${articleData[i].date.split('T')[0]})</li>`;
+                }
+                content += "</ul>"
+                articles.innerHTML = content;
+            } else {
+                articles.innerHTML('Oops! Could not load all articles!')
+            }
+        }
+    };
     
-}  
-    
-  // The first thing to do is to check if the user is logged in!
+    request.open('GET', '/get-articles', true);
+    request.send(null);
+}
+
+
+// The first thing to do is to check if the user is logged in!
 loadLogin();
 
 // Now this is something that we could have directly done on the server-side using templating too!
