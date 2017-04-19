523d2310fc3ad1dd31691ddf266d504d12c03757
ujjwalchaudhary <uchaudhary244@gmail.com>
2017-04-19 18:12:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -154,6 +154,16 @@ app.get('/logout', function (req, res) {
    res.send('<html><body>Logged out!<br/><br/><a href="/">Back to home</a></body></html>');
 });
 
+('#myTabs a').click(function (e) {
+  e.preventDefault()
+  $(this).tab('show')
+});
+
+('#myTabs a[href="#profile"]').tab('show') // Select tab by name
+('#myTabs a:first').tab('show') // Select first tab
+('#myTabs a:last').tab('show') // Select last tab
+('#myTabs li:eq(2) a').tab('show') // Select third tab (0-indexed)
+
 "('.carousel').carousel()";
 
 ('.carousel').carousel({
