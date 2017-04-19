7da3b8405d35b2cd457189627277a0caa94fb16d
ujjwalchaudhary <uchaudhary244@gmail.com>
2017-04-19 15:23:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -182,17 +182,8 @@ app.get('/articles/:articleName', function(req, res){
     
    
 
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-
-app.get('/ui/main.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
-});
-
-
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+app.get('/ui/:fileName', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', req.params.fileName));
 });
 
 
