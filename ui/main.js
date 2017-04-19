24efffb5a5f5e3059d462899759400d29bb2c394
ujjwalchaudhary <uchaudhary244@gmail.com>
2017-04-19 16:20:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,8 +7,9 @@ function loadLoginForm () {
         <input type="submit" id="login_btn" value="Login" />
         <input type="submit" id="register_btn" value="Register" />
         `;
-    document.getElementById('login_area').innerHTML = loginHtml;
-    
+        
+         document.getElementById('login_area').innerHTML = loginHtml;
+         
     // Submit username/password to login
     var submit = document.getElementById('login_btn');
     submit.onclick = function () {
